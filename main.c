c0b01b4803c307ad0bc1be5c51c3ddc23f79a9c6
Zach Riane Machacon <zachriane01@gmail.com>
2024-04-25 08:16:01 +0800

    feat: added reading of lines from file

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#define FILE_PATH "./data/assembly.asm"
+
+int getLinesFromFile(FILE *currentFile, char **lines);
+
+int main()
+{
+    char *lines[1024];
+    FILE *currentFile = fopen(FILE_PATH, "r");
+    int numLines = getLinesFromFile(currentFile, lines);
+    if (numLines > 0)
+    {
+        for (int i = 0; i < numLines; i++)
+        {
+            printf("%s\n", lines[i]);
+        }
+    }
+    else
+    {
+        printf("[ERROR] Unable to open file %s or file is empty.\n", FILE_PATH);
+    }
+}
+
+int getLinesFromFile(FILE *currentFile, char **lines)
+{
+    if (currentFile != NULL)
+    {
+        int numLines = 0;
+        char line[1024];
+        while (fgets(line, sizeof(line), currentFile) != NULL)
+        {
+            line[strcspn(line, "\n")] = '\0'; // Replace \n in files with \0 null terminator.
+            lines[numLines++] = strdup(line); // Copy string to lines array
+        }
+        fclose(currentFile);
+        return numLines;
+    }
+    else
+    {
+        return 0;
+    }
+}
