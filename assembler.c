76434cd718f5c5b88c026819da68ad6311f7707f
Zach Riane Machacon <zachriane01@gmail.com>
2024-04-25 13:19:45 +0800

    feat: implemented instruction and operand string extraction without branches

--- assembler.c
+++ assembler.c
@@ -23,6 +23,7 @@ const INSTRUCTION INSTRUCTION_TO_HEX_VALUES[] = {
 
 int assembleToC(char **assembledLines, char **extractedLines, int numLines)
 {
+    int i;
     int eopExists = checkIfEOPExists(extractedLines, numLines);
     unsigned int startingAddress = getStartingAddress(extractedLines, numLines);
     if (eopExists && startingAddress < 0xFFFF)
@@ -30,9 +31,12 @@ int assembleToC(char **assembledLines, char **extractedLines, int numLines)
         char **branchKeys = malloc(numLines * sizeof(char *));
         unsigned int *branchValues = malloc(numLines * sizeof(unsigned int));
         int numBranches = getBranches(extractedLines, numLines, branchKeys, branchValues, startingAddress);
-        
-
-        return 1;
+        int numAssembledLines = convertAsmToC(assembledLines, extractedLines, numLines, numBranches, branchKeys, branchValues, startingAddress);
+        // for (i = 0; i < numAssembledLines; i++)
+        // {
+        //     printf("%s\n", assembledLines[i]);
+        // }
+        return numAssembledLines;
     }
     else
     {
@@ -118,4 +122,128 @@ int getBranches(char **extractedLines, int numLines, char **branchKeys, unsigned
         startingAddress += 2;
     }
     return numBranches;
+}
+
+int convertAsmToC(char **assembledLines, char **extractedLines, int numLines, int numBranches, char **branchKeys, unsigned int *branchValues, unsigned int startingAddress)
+{
+    int i, j, k, numAssembledLines = 0;
+
+    unsigned int *currentAddress = &startingAddress;
+    char *token;
+    char tempLine[256];
+    for (i = 1; i < numLines; i++)
+    {
+        int validAddress = 1;
+        unsigned int instruction, operand;
+        strcpy(tempLine, extractedLines[i]);
+        token = strtok(tempLine, " \t");
+
+        if (!validAddress)
+        {
+            return 0;
+        }
+
+        while (token != NULL)
+        {
+            for (j = 0; j < NUM_INSTRUCTIONS; j++)
+            {
+                if (strcmp(token, INSTRUCTION_TO_HEX_KEYS[j]) == 0)
+                {
+                    instruction = INSTRUCTION_TO_HEX_VALUES[j].opcode;
+                    char *stringOperand = strtok(NULL, " \t");
+                    int parsed = sscanf(stringOperand, "%x", &operand);
+                    validAddress = checkValidAddress(token, operand);
+                    if (INSTRUCTION_TO_HEX_VALUES[j].needs_adding && parsed == 1)
+                    {
+                        unsigned int current_instruction = (instruction << 8) + operand;
+                        instruction = (current_instruction >> 8) & 0xFF;
+                        operand = current_instruction & 0xFF;
+                    }
+                    else if (parsed != 1)
+                    {
+                        int found = 0;
+                        for (k = 0; k < numBranches; k++)
+                        {
+                            if (strcmp(stringOperand, branchKeys[k]) == 0)
+                            {
+                                operand = branchValues[k];
+                                found = 1;
+                                break;
+                            }
+                        }
+                        if (!found)
+                        {
+                            operand = 0x00;
+                        }
+                    }
+                    token = NULL;
+                    assembledLines[numAssembledLines++] = getInstructionString(instruction, currentAddress);
+                    assembledLines[numAssembledLines++] = getInstructionString(operand, currentAddress);
+                    printf("%s\n", extractedLines[i]);
+                    printf("CURRENT: %s\n", branchKeys[0]);
+                    printf("CURRENT: %s\n", branchKeys[1]);
+                    break;
+                }
+            }
+            if (token != NULL)
+            {
+                token = strtok(NULL, " \t");
+            }
+        }
+    }
+    return numAssembledLines;
+}
+
+int checkValidAddress(char *instruction, unsigned int operand)
+{
+    if (strcmp(instruction, "WIO") == 0 || strcmp(instruction, "RIO") == 0)
+    {
+        return operand >= 0x000 && operand <= 0x01F;
+    }
+    else if (strcmp(instruction, "WM") == 0 || strcmp(instruction, "RM") == 0)
+    {
+        return operand >= 0x400 && operand <= 0x7FF;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+char *getInstructionString(unsigned int instruction, unsigned int *currentAddress)
+{
+    char buffer[50];
+    int max_len = sizeof(buffer);
+
+    int len = snprintf(buffer, max_len, "ADDR=0x%02X;BUS=0x%02X;MainMemory();", *currentAddress, instruction);
+    (*currentAddress)++;
+    if (len >= 0 && len < max_len)
+    {
+        char *result = malloc(len + 1);
+        if (result)
+        {
+            strcpy(result, buffer);
+            return result;
+        }
+    }
+    return NULL;
+}
+
+char *getOperandString(unsigned int operand, unsigned int *currentAddress)
+{
+    char buffer[50];
+    int max_len = sizeof(buffer);
+
+    int len = snprintf(buffer, max_len, "ADDR=0x%02X;BUS=0x%02X;MainMemory();", *currentAddress, operand);
+    (*currentAddress)++;
+    if (len >= 0 && len < max_len)
+    {
+        char *result = malloc(len + 1);
+        if (result)
+        {
+            strcpy(result, buffer);
+            return result;
+        }
+    }
+    return NULL;
 }
